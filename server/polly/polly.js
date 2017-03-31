eb82b9d47dfad6d898b12c0aaa1595378f79d08e
Shkurpylo <anatolii.shkurpylo@gmail.com>
2017-03-31 19:27:49 +0300

    separate aws_config

--- server/polly/polly.js
+++ server/polly/polly.js
@@ -1,18 +1,15 @@
 import AWS from 'aws-sdk';
 import path from 'path';
-import conf from '../../config.json';
 
 AWS.config.loadFromPath(getConfigPath());
 
 const Polly = new AWS.Polly({
   signatureVersion: 'v4',
-  region: conf.credentials.region || 'us-east-1',
 });
 
 export default Polly;
 
 function getConfigPath(){
-  let configPath = path.join(__dirname, '../..', 'config.json');
-
+  let configPath = path.join(__dirname, '../../', 'aws_config.json');  // path work on RPI, for ubuntu try (__dirname, '../../', 'aws_config.json')
   return configPath.toString();
 }
